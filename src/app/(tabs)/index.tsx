a601a7721a0c37a914e5fa44b73e4be00d00957f
Marcelo Costa <marcelodscosta@yahoo.com.br>
2024-02-13 19:06:36 -0300

    customizing color palette

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function Home() {
   return (
-    <View className="flex-1 ">
+    <View className="flex-1 bg-gray-900">
       <Text>Home</Text>
     </View>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+import { colors } from './src/styles/colors';
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors,
+    },
   },
   plugins: [],
 }
