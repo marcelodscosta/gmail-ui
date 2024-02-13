b9285520844789a5e3efd0dfb0b666c57cf791b6
Marcelo Costa <marcelodscosta@yahoo.com.br>
2024-02-13 19:22:51 -0300

    configuring font loading

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,5 +1,12 @@
 import '@/styles/global.css';
 
+import {
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto';
+
 import { Slot } from 'expo-router';
 
 import { StatusBar } from 'react-native';
@@ -7,6 +14,14 @@ import { StatusBar } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function Layout() {
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar barStyle={'dark-content'} />
