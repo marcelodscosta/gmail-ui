9a567b814afe4a309dbb9314f3e722b1334f772e
Marcelo Costa <marcelodscosta@yahoo.com.br>
2024-02-13 19:13:47 -0300

    customizing status bar

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,10 +1,15 @@
 import '@/styles/global.css';
+
 import { Slot } from 'expo-router';
+
+import { StatusBar } from 'react-native';
+
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function Layout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
+      <StatusBar barStyle={'dark-content'} />
       <Slot />
     </GestureHandlerRootView>
   );
