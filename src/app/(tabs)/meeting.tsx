a20eb4c4052c10e3bf43a4a7180a0125282dfae5
Marcelo Costa <marcelodscosta@yahoo.com.br>
2024-02-18 18:02:01 -0300

    creating menu tabs

--- src/app/(tabs)/meeting.tsx
+++ src/app/(tabs)/meeting.tsx
@@ -4,7 +4,7 @@ export default function Meeting() {
   return (
     <View className="flex-1 bg-gray-900 pt-14 items-center justify-center">
       <Text className="text-white text-2xl font-heading">
-        Chat
+        Meeting
       </Text>
     </View>
   );
