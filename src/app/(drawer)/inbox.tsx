1aedfe2797c12a42d84be12e6859413fe04ae109
Marcelo Costa <marcelodscosta@yahoo.com.br>
2024-03-03 21:08:54 -0300

    creating routes

---
+++ src/app/(drawer)/inbox.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Inbox() {
+  return (
+    <View className="flex-2 bg-gray-900 items-center justify-center">
+      <Text className="text-white text-2xl font-heading">Caixa de Entrada</Text>
+    </View>
+  );
+};
